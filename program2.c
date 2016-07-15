4772587a4c4f21dd0a192d153e429a84b5f3d3c3
jesseakt <jesseakt@gmail.com>
2016-07-15 09:36:53 -0700

    Updated the mechanic for room connections so they're always reciprocal

--- program2.c
+++ program2.c
@@ -14,6 +14,7 @@ int main(void)
     char fileNames[6][7];   // Array of filenames for rooms in directory
     char *roomNames[10];    // Array of possible room names
     int chosenRooms[7];     // Array of actually selected room choices
+    int connectionArr[7][7];// Array of connections between rooms
     int startRoom;          // chosenRooms index of Start Room
     int endRoom;            // chosenRooms index of End Room
     FILE *currentFile;      // Pointer to the current file
@@ -49,8 +50,9 @@ int main(void)
         //Loop until we get a new random selection
         while(1)
         {
-            //Chose a new candidate number
+            //Choose a new candidate number
             int candidateNum = rand()%10;
+            
             //Loop through the existing numbers to see if we already have it
             int found = 0;
             for(j=0; j<i; j++)
@@ -77,6 +79,48 @@ int main(void)
         endRoom = rand()%7;
     }while(startRoom == endRoom);
 
+    //Choose connections
+    //Initialize connection Array with 0s.
+    for(i = 0; i < 7; i++)
+    {
+        for(j = 0; j < 7; j++)
+        {
+            connectionArr[i][j] = 0;
+        }
+    }
+    /* Loop through, randomly select rooms to form connections
+     * continue on if there are 3 connections to the room.
+     * 0 if no connection, 1 if connection
+     * The self connection (connectionArr[n][n]) holds the number
+     * of connections that room n has to other rooms.*/
+    for(i = 0; i < 7; i++)
+    {
+        //Stay in this step of the for loop until there are 3 or more conn.
+        while(connectionArr[i][i] < 3)
+        {
+            //Choose a new candidate connection (not same room)
+            int newconn;
+            do{
+                newconn = rand()%7;
+            }while(newconn == i);
+            
+            /*If connectionArr[i][newconn]==0, it's a new connection
+             * so we need to add the new connection in and update the 
+             * connection counters. If it's equal to 1, it's an existing
+             * connection so do nothing.*/
+            if(connectionArr[i][newconn]==0)
+            {
+                //Make association and reciprocal association
+                connectionArr[i][newconn] = 1;
+                connectionArr[newconn][i] = 1;
+                //Increment connection counters for room i and room newconn
+                connectionArr[i][i] +=1;
+                connectionArr[newconn][newconn] +=1;
+            }
+        }
+    }
+
+
     //Make and Populate a new room file for each of the rooms.
     for(i = 0; i < 7; i++)
     {
@@ -90,41 +134,17 @@ int main(void)
         fprintf(currentFile, "ROOM NAME: %s\n", roomNames[chosenRooms[i]]);
 
         //Write the room connections
-        //Choose a random number of extra connections (3-6)
-        //Choose a random number between 0-3 and add 3.
-        int extraConnections = rand()%4 + 3;
-        //Define an array of room indices to use as extra connections.
-        int newConnections[extraConnections];
-        int candidateNew; //Used to hold the candidate new value
-        //Select "extraConnections" unique room indices
-        for(j=0; j<extraConnections; j++)
+        k = 1; //connection counter
+        //Iterate through connectionArr[i][x] and write existing connections
+        for(j = 0; j < 7; j++)
         {
-            //Loop until we actually add a unique index.
-            while(1)
+            //If we have a connection and we're not looking at the same room
+            if(connectionArr[i][j] == 1 && i != j)
             {
-                //Get a new index that isn't the room we're in.
-                do{
-                    candidateNew = rand()%7;
-                }while(candidateNew == i);
-                //Loop through existing indices to see if we aready have it
-                int found = 0;
-                for(k=0; k<j; k++)
-                {
-                    //If we find it flag that it's already here
-                    if(candidateNew == newConnections[k])
-                        found = 1;
-                }
-                
-                //If we didn't find it, add it to the list
-                if(found != 1)
-                {
-                    newConnections[j] = candidateNew;
-                    //escape the while loop after adding
-                    break;
-                }
+                fprintf(currentFile, "CONNECTION %d: %s\n", k, roomNames[chosenRooms[j]]);
+                //Increment the connection counter
+                k++;
             }
-            //Add new index to the connections list.
-            fprintf(currentFile, "CONNECTION %d: %s\n", j+1, roomNames[chosenRooms[newConnections[j]]]);
         }
 
 
